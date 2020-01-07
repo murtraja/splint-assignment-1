ff85ff30a8140054aca9ab1963c4b3a9c08799fc
Murtaza Raja <murt.raja@gmail.com>
2020-01-07 11:27:07 +0530

    down to 1 warning

--- code-1.c
+++ code-1.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+
+/*@null@*/ /*@out@*/ static char* foo() {
+    char *p = (char*) malloc(12*sizeof(char));
+    return p;
+}
+
 int main () {
     char *p;
     int x;
     char s;
-    char* foo();
+    // char* foo();
     printf("Please enter the value of x\n");
     (void)scanf ("%d", &x);
     if (x==0)
@@ -11,8 +17,10 @@ int main () {
     else
         p = foo();
 
-    if(x !=0)
+    if(x !=0 && p!= 0) {
         s=*p;
+        free(p);
+    }
 
     else
     printf("The value of x entered is %d\n",x) ;
