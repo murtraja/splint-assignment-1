75a090fbfb3cb77e5c604a9b51693e0208a48582
Murtaza Raja <murt.raja@gmail.com>
2020-01-07 11:15:19 +0530

    scanf value return error fix

--- code-1.c
+++ code-1.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 int main () {
-char *p;
-int x;
-char s;
-char* foo();
-printf("Please enter the value of x\n");
-scanf ("%d", &x);
-if ( x==0)
-p = 0 ;
+    char *p;
+    int x;
+    char s;
+    char* foo();
+    printf("Please enter the value of x\n");
+    (void)scanf ("%d", &x);
+    if (x==0)
+        p = 0 ;
+    else
+        p = foo();
 
-else
-p = foo() ;
-if(x !=0)
-s=*p;
+    if(x !=0)
+        s=*p;
 
-else
-printf("The v al u e o f x e n t e r e d i s %d\n", x ) ;
-return 0;
+    else
+    printf("The value of x entered is %d\n",x) ;
+    return 0;
 
-}
+    }
